f33b141f1e27176e48016437c4a0cada6f4d4885
meamuri <meammf@yandex.ru>
2020-10-10 17:09:43 +0300

    Draft iterating design: dayly granularity and raw UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    let instant = new Date()
+    let forRender = []
+    for (let i = 0; i < 100; i++) {
+        let info = {
+            date: instant.toISOString(),
+            unix: instant.getTime(),
+            zone: "Europe/Moscow"
+        }
+        forRender.push(info)
+        instant = new Date(info.unix - 86400 * 1000)
+    }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {forRender.map(e =>
+              <p>
+                  Time: {e.date}; Unix: {e.unix}
+              </p>
+          )}
       </header>
     </div>
   );
